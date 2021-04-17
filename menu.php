1c49ec0ccfa4a42e7ce1af3f95dda02d5a6a2e56
ljrivel <ljrivel@gmail.com>
2021-04-17 00:46:31 -0600

    Arreglos menu

--- menu.php
+++ menu.php
@@ -4,17 +4,58 @@
 
 <head>
 	<title>Inicio</title>
-	<link rel="stylesheet" href="login.css">
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-	<meta name="generator" content="Geany 1.36" />
+
+	<style >
+		.body {
+  			background: #EC610B;
+ 			 min-height: 100vh;
+		}
+
+		.h2 {
+  			text-align: center;
+  			margin: 0px auto;
+  			margin-bottom: 10px;
+  			color: rgba(0, 0, 0, 0.5);
+  			font-family: Arial Black;
+		}
+
+		.form {
+ 			 margin: auto;
+  			width: 50%;
+  			max-width: 500px;
+  			background: #F3F3F3;
+  			padding: 20px;
+  			border: 1px solid rgba(0, 0, 0, 0.2);
+		}
+
+		.boton{
+             background-color: #FFDA63;
+             border: none;
+             color: black;
+             padding: 15px 32px;
+             text-align: center;
+             text-decoration: none;
+             display: inline-block;  
+             font-size:22px;
+             font-family: Arial;
+  			 border-radius: 20px;
+             margin: 0px auto;
+         }
+		.botonAzul{
+             background-color: #008CBA;
+		}
+
+	</style>
+
 </head>
 
-<body>
-	
-		<form method="post">
-        <h2>Menú Principal</h2>
-        <button onclick="location.href='MostrarPuestos.php'">Ver puestos</button><br>
-        <button onclick="location.href='MostrarEmpleados.php'">Ver empleados</button>
+<body class="body">
+	    <br>
+        <h2 class="h2">Menú Principal</h2>
+        <br>
+        <button class="boton" onclick="location.href='MostrarPuestos.php'">Puestos &#128273</button>
+        <button class="boton botonAzul" onclick="location.href='MostrarEmpleados.php'">&#128119Empleados</button>
+
 	
 </body>
 
